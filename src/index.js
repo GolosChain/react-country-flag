466e31aac0a8810b3db58301496c4110df47f88b
Alexey Elizarov <felex-ae@ya.ru>
2020-09-04 17:32:14 +0300

    feat: fork version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ReactCountryFlag extends Component {
     static propTypes = {
@@ -7,57 +7,37 @@ export default class ReactCountryFlag extends Component {
         code: PropTypes.string.isRequired,
         styleProps: PropTypes.object,
         svg: PropTypes.bool,
-        title: PropTypes.string
+        title: PropTypes.string,
     };
 
     static defaultProps = {
-        cdnUrl:
-            "https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.1/flags/4x3/",
-        svg: false
+        cdnUrl: 'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.1/flags/4x3/',
+        svg: false,
     };
 
     render() {
-        const { cdnUrl, code, styleProps, svg, title } = this.props;
+        const { cdnUrl, code, styleProps, className } = this.props;
 
         const flagUrl = `${cdnUrl}${code.toLowerCase()}.svg`;
-        const emoji = code
-            .toUpperCase()
-            .replace(/./g, char =>
-                String.fromCodePoint(char.charCodeAt(0) + 127397)
-            );
 
-        return svg ? (
-            <span
+        return (
+            <img
+                src={flagUrl}
                 aria-label={code}
-                role="img"
+                alt={code}
                 style={{
-                    position: "relative",
-                    display: "inline-block",
-                    width: "1.3333333333em",
-                    height: "1em",
-                    backgroundImage: `url(${flagUrl})`,
-                    backgroundPosition: "50%",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "contain",
-                    fontSize: "1em",
-                    lineHeight: "1em",
-                    verticalAlign: "middle",
-                    ...styleProps
+                    position: 'relative',
+                    display: 'inline-block',
+                    objectPosition: '50%',
+                    backgroundRepeat: 'no-repeat',
+                    objectFit: 'contain',
+                    fontSize: '1em',
+                    lineHeight: '1em',
+                    verticalAlign: 'middle',
+                    ...styleProps,
                 }}
-                title={title || code}
+                className={className}
             />
-        ) : (
-            <span
-                aria-label={code}
-                role="img"
-                style={{
-                    verticalAlign: "middle",
-                    ...styleProps
-                }}
-                title={code}
-            >
-                {emoji}
-            </span>
         );
     }
 }
